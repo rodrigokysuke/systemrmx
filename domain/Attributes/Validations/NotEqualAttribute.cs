e096b00ead2fe526eeb41cf85e16a5ec36a82746
Luiz de Queiroz <oluizdequeiroz@gmail.com>
2021-04-28 17:46:11 -0300

    criando atributo NotEqual p/ validação de propriedade.

---
+++ domain/Attributes/Validations/NotEqualAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace domain.Attributes.Validations
+{
+    public class NotEqualAttribute : ValidationAttribute
+    {
+        private readonly object value;
+
+        public NotEqualAttribute(object value)
+        {
+            this.value = value;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            switch (this.value.GetType().Name)
+            {
+                case "Int32": return (int)value == (int)this.value ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+                case "String": return (string)value == (string)this.value ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+                case "Double": return (double)value == (double)this.value ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+                case "Char": return (char)value == (char)this.value ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+                default: return (int)value == (int)this.value ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+            }
+        }
+    }
+}
